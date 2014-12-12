Fixed probability distribution units

src/Hipobject.py
@@ -121,20 +121,22 @@ class HIPobjs(object):
                 #get age range of the age
                 tmax=max(tdata[:,1])
                 index1 = tdata[:,0]>tdata[:,0][tdata[:,1]==tmax]
-                index2 = tdata[:,1]<1.e-5
+                index2 = tdata[:,1]<1.e-4
                 #print tmax,len(tdata[:,1][index2])
                 #print len(tdata[:,0][index1*index2])
                 #print len(tdata[:,0][(-index1)*index2])
                 xmax = tdata[:,0][index1*index2][0]
                 xmin = tdata[:,0][(-index1)*index2][0]
                 #print xmin, xmax
-                ax1.plot(10**tdata[:,0]/1.e6,tdata[:,1])
-                ax1.set_xlim([10.**xmin/1.e6,10.**xmax/1.e6])
+                #ax1.plot(10**tdata[:,0]/1.e6,tdata[:,1])
+                #ax1.set_xlim([10.**xmin/1.e6,10.**xmax/1.e6])
+                ax1.plot(tdata[:,0],tdata[:,1])
+                ax1.set_xlim([xmin,xmax])
                 #print 10.**xmin, 10.**xmax
                 ax1.set_ylabel("dp/dT(x constant)")
                 ax1.set_xlabel("Myr")
                 ax2.set_ylabel("dp/dz(x constant)")
-                ax2.set_xlabel("FeH")
+                ax2.set_xlabel("Z")
                 zdata = np.loadtxt(zfile)
                 zmax=max(zdata[:,1])
                 ax2.plot(zdata[:,0],zdata[:,1])

src/calc_probs.py
@@ -142,7 +142,8 @@ def calcprobs(star, FeHval=0, dFeH=0.13, norm=False, rot=True):
 
     outarr = np.zeros((pZ.shape[0], 2))
     outarr[:, 0] = Z
-    outarr[:, 1] = pZ
+    outarr[:, 1] = pZ/(Z_bins[1:] - Z_bins[:-1])
+    outarr[:, 1] /= np.amax(outarr[:, 1])
     zmax=Z[pZ==max(pZ)]
     zerr=0
     if rot:
@@ -159,8 +160,11 @@ def calcprobs(star, FeHval=0, dFeH=0.13, norm=False, rot=True):
     pT = np.histogram(logT_corr, bins=T_bins, weights=pdens)[0]
         
     outarr = np.zeros((pT.shape[0], 2))
-    outarr[:, 0] = logT
-    outarr[:, 1] = pT
+    #outarr[:, 0] = logT
+    #outarr[:, 1] = pT
+    outarr[:, 0] = 10**(logT - 6)
+    outarr[:, 1] = pT/outarr[:, 0]
+    outarr[:, 1] /= np.amax(outarr[:, 1])
     tmax=10.**logT[pT==max(pT)]/1.e6
     terr=0
     if rot: